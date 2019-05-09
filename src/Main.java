335b7137dbc91dce9c88caed99a3ae371370ffc8
Sebastian Madejski <34239101+mwht@users.noreply.github.com>
2019-05-09 17:34:28 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("+-----------+\n| SpiceTech |\n+-----------+\n\nLeader GitHub: @mwht (https://github.com/mwht)\nDeveloper GitHub: @BokononPL (https://github.com/BokononPL)");
-        System.out.println("+-----------+\n| SpiceTech |\n+-----------+\n\nLeader GitHub: @mwht (https://github.com/mwht)");
         System.out.println("--- C R E D I T S : ---");
         System.out.println("Operations: Sebastian Madejski (@mwht)");
         System.out.println("Tester Github: @Lukkario (https://github.com/Lukkario)");
