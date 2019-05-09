f8d942598e9d4715f69c7a190d85a5e22fe3bd9f
BokononPL <zalozonynaszybko@gmail.com>
2019-05-09 18:36:25 +0200

    Added username to message

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("+-----------+\n| SpiceTech |\n+-----------+\n\nLeader GitHub: @mwht (https://github.com/mwht)");
+        System.out.println("+-----------+\n| SpiceTech |\n+-----------+\n\nLeader GitHub: @mwht (https://github.com/mwht)\nGitHub: @BokononPL (https://github.com/BokononPL)");
     }
 
 }
