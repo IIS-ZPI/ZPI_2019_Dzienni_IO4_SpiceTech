18678652c583deebdcfd3e82a427c9725c953692
Anon <zalozonynaszybko@gmail.com>
2019-06-02 16:05:45 +0200

    reduced the number of options to 6 to reflect new convention, as described in backlog

--- src/read_operation.py
+++ src/read_operation.py
@@ -4,11 +4,9 @@ def read_operation():
     2 - mode\n\
     3 - standard deviation\n\
     4 - coefficient of variation\n\
-    5 - rising period (???)\n\
-    6 - falling period (???)\n\
-    7 - constant period (???)\n\
-    8 - comparison (???)\n"))
-    if op not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+    5 - periods of change\n\
+    6 - comparison\n"))
+    if op not in ["1", "2", "3", "4", "5", "6"]:
         print("Invalid choice!")
         op = None
     return op
