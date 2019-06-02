7429d0c1dffe2822f57beed6c16ed8bc51b29fef
hexenisalive <6stringbastard@gmail.com>
2019-06-02 21:32:28 +0200

    Fix line line length

--- src/get_period_data.py
+++ src/get_period_data.py
@@ -12,7 +12,8 @@ def get_period_data(period, table_code, currency_code):
         if i != iterations-1:
             temp_period = period + relativedelta(days=93)
             print("start: {0}, end: {1}, num_of_days: {2}".format(period, temp_period, temp_period - period))
-            url = str("http://api.nbp.pl/api/exchangerates/rates/{0}/{1}/{2}/{3}/?format=json".format(table_code, currency_code, period, temp_period))
+            url = str("http://api.nbp.pl/api/exchangerates/rates/{0}/{1}/{2}/{3}/?format=json".format(
+                table_code, currency_code, period, temp_period))
             response = requests.get(url)
             if response.status_code != 200:
                 print("Error: request returned code: " + str(response.status_code))
@@ -23,7 +24,8 @@ def get_period_data(period, table_code, currency_code):
                 period = temp_period + relativedelta(days=1)
         else:
             print("start: {0}, end: {1}, num_of_days: {2}".format(period, today, today - period))
-            url = str("http://api.nbp.pl/api/exchangerates/rates/{0}/{1}/{2}/{3}/?format=json".format(table_code, currency_code, period, today))
+            url = str("http://api.nbp.pl/api/exchangerates/rates/{0}/{1}/{2}/{3}/?format=json".format(
+                table_code, currency_code, period, today))
             response = requests.get(url)
             if response.status_code != 200:
                 print("Error: request returned code: " + str(response.status_code))
