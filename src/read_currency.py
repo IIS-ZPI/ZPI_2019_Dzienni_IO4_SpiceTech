929594a2c58314df0dd4f2e4f5c0853f01f73a69
Anon <zalozonynaszybko@gmail.com>
2019-06-02 14:49:03 +0200

    initial commit

---
+++ src/read_currency.py
@@ -0,0 +1,14 @@
+def read_currency(a, b):
+    cc = None
+    tc = None
+    while cc is None:
+        inp = str(input("input 3-letter currency code (eg. USD)\n")).upper()
+        if inp in a:
+            cc = inp
+            tc = "A"
+        elif inp in b:
+            cc = inp
+            tc = "B"
+        else:
+            print("Invalid code!")
+    return cc, tc
