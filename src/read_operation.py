e5cbebe8ad8cee04e5e6b5e710c1988fac459a09
Anon <zalozonynaszybko@gmail.com>
2019-06-02 16:03:30 +0200

    removed the while loop and added missing value "8"

--- src/read_operation.py
+++ src/read_operation.py
@@ -1,15 +1,14 @@
 def read_operation():
-    op = None
-    while op not in ["1", "2", "3", "4", "5", "6", "7"]:
-        op = str(input("please select desired operation\n\
-        1 - median\n\
-        2 - mode\n\
-        3 - standard deviation\n\
-        4 - coefficient of variation\n\
-        5 - rising period (???)\n\
-        6 - falling period (???)\n\
-        7 - constant period (???)\n\
-        8 - comparison (???)\n"))
-        if op not in ["1", "2", "3", "4", "5", "6", "7"]:
-            print("Invalid choice!")
-        return op
+    op = str(input("please select desired operation\n\
+    1 - median\n\
+    2 - mode\n\
+    3 - standard deviation\n\
+    4 - coefficient of variation\n\
+    5 - rising period (???)\n\
+    6 - falling period (???)\n\
+    7 - constant period (???)\n\
+    8 - comparison (???)\n"))
+    if op not in ["1", "2", "3", "4", "5", "6", "7", "8"]:
+        print("Invalid choice!")
+        op = None
+    return op
