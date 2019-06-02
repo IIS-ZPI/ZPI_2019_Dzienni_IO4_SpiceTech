1d40ea593242fa3dee917ace9319276375b86031
Lukkario <michallukianski.0@gmail.com>
2019-06-02 15:23:48 +0200

    Add test_read_currency test

--- test/mainTest.py
+++ test/mainTest.py
@@ -1,11 +1,23 @@
 import unittest
 import sys
 sys.path.append("./src/")
+import read_currency
 
 class MainTest(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True,True)
+
+    def test_read_currency(self):
+        a = ["USD", "GBP"]
+        b = ["PL", "NIS"]
+
+        for i in a:
+            read_currency.input = lambda _: i
+            self.assertEqual(read_currency.read_currency(a, b), (i, "A"))
+
+        for i in b:
+            read_currency.input = lambda _: i
+            self.assertEqual(read_currency.read_currency(a, b), (i, "B"))
+
 
 if __name__ == '__main__':
     unittest.main()
