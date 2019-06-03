516bdb480e23905d794bb05c81f24408755760b0
Anon <zalozonynaszybko@gmail.com>
2019-06-03 22:07:03 +0200

    secured the function against request being denied

--- src/get_codes.py
+++ src/get_codes.py
@@ -4,10 +4,16 @@ import requests
 def get_codes():
     a = []
     b = []
-    info = (requests.get("http://api.nbp.pl/api/exchangerates/tables/A/?format=json")).json()
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/A/?format=json")
+    info = response.json()
+    if response.status_code != 200:
+        return None, None
     for x in info[0]["rates"]:
         a.append(x["code"])
-    info = (requests.get("http://api.nbp.pl/api/exchangerates/tables/B/?format=json")).json()
+    response = requests.get("http://api.nbp.pl/api/exchangerates/tables/B/?format=json")
+    info = response.json()
+    if response.status_code != 200:
+        return None, None
     for x in info[0]["rates"]:
         b.append(x["code"])
     return a, b
