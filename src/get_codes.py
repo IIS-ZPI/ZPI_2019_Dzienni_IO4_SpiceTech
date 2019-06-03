e1f63a846879e5cb466a5457257aa59ac9f23401
Anon <zalozonynaszybko@gmail.com>
2019-06-03 22:16:53 +0200

    added documentation string

--- src/get_codes.py
+++ src/get_codes.py
@@ -2,6 +2,16 @@ import requests
 
 
 def get_codes():
+    """
+    Requests tables containing current rates for all currencies from the NBP server.
+    If the request came across OK (code 200), all 3-letter currency codes are extracted from
+    the two tables (A and B) and parsed into two separate arrays.
+    Intended to be used to obtain a list of all currencies available to be chosen by the user in the program.
+
+    Arguments: none
+
+    Returns: two arrays of strings if the request came across OK, or two None objects otherwise
+    """
     a = []
     b = []
     response = requests.get("http://api.nbp.pl/api/exchangerates/tables/A/?format=json")
