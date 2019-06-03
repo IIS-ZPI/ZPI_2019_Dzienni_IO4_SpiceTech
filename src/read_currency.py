111665d4d01cdd7c1bce565e474a5b467a782086
Anon <zalozonynaszybko@gmail.com>
2019-06-03 22:31:29 +0200

    added documentation string

--- src/read_currency.py
+++ src/read_currency.py
@@ -1,4 +1,12 @@
 def read_currency(a, b):
+    """
+    Reads input from the user and checks if such a currency code exists in the table A, B, or neither of them.
+
+    Arguments: two arrays of strings
+
+    Returns: two strings (currency code and the table it belongs to) if the input was correct, or
+    two None objects otherwise
+    """
     cc = None
     tc = None
     inp = str(input("input 3-letter currency code (eg. USD)\n")).upper()
