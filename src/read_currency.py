08b2f3fe6cda708265986bfc1b8ca03037c18c66
Anon <zalozonynaszybko@gmail.com>
2019-06-03 22:25:33 +0200

    removed loop from the function

--- src/read_currency.py
+++ src/read_currency.py
@@ -1,14 +1,13 @@
 def read_currency(a, b):
     cc = None
     tc = None
-    while cc is None:
-        inp = str(input("input 3-letter currency code (eg. USD)\n")).upper()
-        if inp in a:
-            cc = inp
-            tc = "A"
-        elif inp in b:
-            cc = inp
-            tc = "B"
-        else:
-            print("Invalid code!")
+    inp = str(input("input 3-letter currency code (eg. USD)\n")).upper()
+    if inp in a:
+        cc = inp
+        tc = "A"
+    elif inp in b:
+        cc = inp
+        tc = "B"
+    else:
+        print("Invalid code!")
     return cc, tc
