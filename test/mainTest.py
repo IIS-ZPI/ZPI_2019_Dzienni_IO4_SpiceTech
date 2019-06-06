ff72e802293bdbad344507da6d8387a6a20c35e9
Michał Łukiański <michallukianski@gmail.com>
2019-06-06 17:15:12 +0200

    Add test

--- test/mainTest.py
+++ test/mainTest.py
@@ -1,11 +1,18 @@
 import unittest
 import sys
-sys.path.append("./src/")
+from dateutil.relativedelta import relativedelta
+import datetime
+sys.path.append("../src/")
+import read_period
 
 class MainTest(unittest.TestCase):
 
-    def test(self):
-        self.assertTrue(True,True)
+    def test_read_period(self):
+        read_period.input = lambda _: "10"
+        self.assertEqual(read_period.read_period(), None)
+        read_period.input = lambda _: "1"
 
+        rel = relativedelta(days=7)
+        self.assertEqual(read_period.read_period(), datetime.datetime.now().date() - rel)
 if __name__ == '__main__':
     unittest.main()
