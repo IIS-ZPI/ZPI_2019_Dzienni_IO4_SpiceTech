28d4d023ecc65ff576612c6403e5f7d861eb5e7e
hexenisalive <6stringbastard@gmail.com>
2019-06-13 17:07:00 +0200

    Add rounding to get_currency_pair analysis return value

--- src/get_currency_pair_analysis.py
+++ src/get_currency_pair_analysis.py
@@ -1,3 +1,5 @@
+import numpy
+
 def get_currency_pair_analysis(period_data1, period_data2):
     """
     Analyses the daily changes for rates for two different currencies
@@ -13,4 +15,4 @@ def get_currency_pair_analysis(period_data1, period_data2):
     for data in range(1, len(period_data1)):
         comb_data.append((period_data1[data]*period_data2[data-1])/(period_data1[data-1]*period_data2[data]))
 
-    return comb_data
+    return numpy.around(comb_data, 4)
